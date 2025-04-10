ea03dc177bffe9816dba06fa382eb9d40370c6c1
khetsinghyogi@gmail.com <khetsinghyogi@gmail.com>
2025-04-11 00:35:21 +0530

    Error Removed  Wokring Fine

--- app/src/main/java/com/yogify/study/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yogify/study/ui/home/HomeFragment.kt
@@ -13,6 +13,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
+    //Done on Github
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
